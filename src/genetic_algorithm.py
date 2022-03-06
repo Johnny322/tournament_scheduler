8353cf6accc910c2822ff44c87ce884bf91749fe
Johnny322 <sipoperle@hotmail.com>
2022-03-06 16:59:35 +0100

    Initial commit

---
+++ src/genetic_algorithm.py
@@ -0,0 +1,166 @@
+#import xml.etree.ElementTree as ET
+import random, sys, math
+
+def gen_chromosome(matches, time_slots, sport_list):
+    chromosome = {}
+    toast = list(time_slots.keys())
+    already_used = []
+    buffer = len(toast) - 12
+    for time_slot in range(len(toast)):
+        chromosome[toast[time_slot]] = {}
+        for sport in sport_list:
+
+            gene = random.choices(range(len(matches.keys())))
+
+            if len(already_used) >= len(matches.keys()):
+                if time_slot > buffer:
+                    chromosome[toast[time_slot]][sport] = "Buffer"
+                else:
+                    chromosome[toast[time_slot]][sport] = None
+            else:
+                already_tried = already_used.copy()
+                while ((gene in already_tried) or (matches[gene[0]][2] != sport)) and (len(already_tried)<len(matches.keys())):
+                    # print(matches[gene[0]])
+                    if gene not in already_tried:
+                        already_tried.append(gene)
+                    gene = random.choices(range(len(matches.keys())))
+                if(len(already_tried)>=len(matches.keys())):
+                    if time_slot > buffer:
+                        chromosome[toast[time_slot]][sport] = "Buffer"
+                    else:
+                        chromosome[toast[time_slot]][sport] = None
+                else:
+                    # print(matches[gene[0]][2], ' == ', sport)
+                    # print('Appending ', gene[0], ' to already used, which is ', matches[gene[0]])
+                    already_used.append(gene)
+                    # print('Already used = ', already_used)
+                    # print(time_slot, ', ', sport, ' now contains ', matches[gene[0]])
+                    chromosome[toast[time_slot]][sport] = gene
+    # print('done')
+    return chromosome
+
+def gen_pop(pop_size,matches,time_slots, sport_list):
+    return [gen_chromosome(matches, time_slots, sport_list) for _ in range(pop_size)]
+
+def pretty_print_dict(dic):
+    for d in dic.items():
+        print(d)
+    print("\n")
+
+def find_least_fit(fitness_scores):
+    fitness_scores_values = list(fitness_scores.values())
+    fitness_scores_values_copy = list(fitness_scores_values)
+
+    least_fittest_value = min(fitness_scores_values)
+    least_fittest = fitness_scores_values.index(least_fittest_value)
+    fitness_scores_values.remove(least_fittest_value)
+
+    second_least_fittest_value = min(fitness_scores_values)
+    second_least_fittest = fitness_scores_values_copy.index(second_least_fittest_value)
+    return least_fittest, second_least_fittest
+
+def select(fitness_scores):
+    fitness_scores_values = list(fitness_scores.values())
+    fitness_scores_values_copy = list(fitness_scores_values)
+    fittest_value = min(fitness_scores_values)
+    fittest = fitness_scores_values.index(fittest_value)
+    fitness_scores_values.remove(fittest_value)
+    second_fittest_value = min(fitness_scores_values)
+    second_fittest = fitness_scores_values_copy.index(second_fittest_value)
+    return fittest,second_fittest
+
+def crossover(parent1, parent2):
+    if len(parent1) != len(parent2):
+        raise ValueError("Genomes did not have same length")
+    length = 3
+    crossover_integer = random.randint(0, length - 1)
+    child1 = {}
+    child2 = {}
+    sports_list = list(parent1[0].keys())
+    for i in range(len(parent1)):
+        child1[i] = parent1[i]
+        child1[i][sports_list[crossover_integer]] = parent2[i][sports_list[crossover_integer]]
+        child2[i] = parent2[i]
+        child2[i][sports_list[crossover_integer]] = parent1[i][sports_list[crossover_integer]]
+
+    return child1, child2
+
+def mutation(chromosome,sport_list, mutate_probability = 0.5):
+    for i in range(1000):
+        index1 = random.randrange(len(chromosome))
+        index2 = random.randrange(len(chromosome))
+        index_list = random.randrange(len(sport_list))
+        if not(random.random() > mutate_probability):
+            while (chromosome[index1][sport_list[index_list]] == "Buffer" or chromosome[index2][sport_list[index_list]] == "Buffer"):
+                index1 = random.randrange(len(chromosome))
+                index2 = random.randrange(len(chromosome))
+            temp = chromosome[index1][sport_list[index_list]]
+            chromosome[index1][sport_list[index_list]] = chromosome[index2][sport_list[index_list]]
+            chromosome[index2][sport_list[index_list]] = temp
+    return chromosome
+
+def calculate_interference(task, sorted_task_dict, prev_task_list, wcet_factor):
+    D_deadline, T_period, T_wcet, cpu_id, core_id, wcrt = task
+    if prev_task_list == []:
+        return T_wcet*wcet_factor, T_period, T_wcet
+    last_task = prev_task_list[len(prev_task_list)-1]
+    prev_task_list.remove(last_task)
+    P_interference, P_period, P_wcet = calculate_interference(sorted_task_dict[last_task], sorted_task_dict, prev_task_list, wcet_factor)
+    interference = T_wcet*wcet_factor + math.ceil(P_interference/P_period)*P_wcet*wcet_factor #computes interferencecalculate_interference()
+    return interference, T_period, T_wcet
+
+def create_sorted_task_dict(tasks_for_core, tasks, wcet_factor, cpu_id, core_id):
+    task_dict = {}
+    for gene_id in tasks_for_core:
+        task = tasks[gene_id]
+        deadline, period, wcet, task_id = int(task["Deadline"]), int(task["Period"]), int(task["WCET"]), int(task["Id"])
+        task_dict[gene_id] = [deadline, period, wcet, cpu_id, core_id, 0]
+    sorted_task_list = sorted(task_dict.items(), key = lambda x : x[1][0])
+    sorted_task_dict = {i : l for i,l in sorted_task_list}
+    return sorted_task_dict
+
+#compute fitness values to optimize solution to problem
+def fitness(population, matches):
+    fitness_scores = {}
+    # gene_ids = list(map(int,list(tasks.keys())))
+    fitness_chromo_dict = {}
+    overlaps = {}
+    for i in range(len(population)): #chromosome encodes a solution (list of core id's)
+        overlaps[i] = {}
+        counter = 0
+        total_fitness = 0
+        prev_slot = []
+        for time_slot in population[i]:
+            # print('timeslot = ',chromosome[time_slot])
+            team_list = []
+            for entry in population[i][time_slot]:
+                if(population[i][time_slot][entry] != None and population[i][time_slot][entry] != "Buffer"):
+                    a = population[i][time_slot][entry][0]
+                    # print('match_id = ',i)
+                    # print('added ', matches[i])
+                    team_list.append(matches[a][0])
+                    team_list.append(matches[a][1])
+                    # print('added ', matches[i][1])
+            # print('team_list = ',team_list)
+            # print('len of team_list = ',len(team_list))
+            # print('len of team_set = ',len(set(team_list)))
+            if(len(team_list)>len(set(team_list))):
+                # print("This not good")
+                total_fitness += 10000
+            if counter % 4 != 0:
+                if(len(prev_slot)>0):
+                    # print("prev_slot = ",prev_slot)
+                    # print("team_list = ",team_list)
+                    for team in team_list:
+                        if team in prev_slot:
+                            # print("team ",team, " is in prevlist and is added to overlaps")
+                            if time_slot in overlaps[i].keys():
+                                overlaps[i][time_slot].append(team)
+                            else:
+                                overlaps[i][time_slot] = [team]
+                            total_fitness += 10
+            counter+=1
+            prev_slot = team_list
+        # print("")
+        fitness_scores[i] = total_fitness
+    return fitness_scores, overlaps
---
+++ src/main.py
@@ -0,0 +1,127 @@
+#External Modules/libraries
+import xml.etree.ElementTree as ET
+import random, sys, math
+from bs4 import BeautifulSoup
+import os
+
+#Selfwritten
+import genetic_algorithm as ga
+
+def gen_sport_dict(teams):
+    sport_dict = {}
+    for team in teams:
+        #Info
+        id = team.get('Id')
+        group_id = team.get('Group_id')
+        sport = team.get('Sport')
+        if sport not in sport_dict:
+            sport_dict[sport] = {}
+        if group_id not in sport_dict[sport]:
+            sport_dict[sport][group_id] = []
+
+        sport_dict[sport][group_id].append(id)
+    return sport_dict
+
+def gen_timeslot_dicts(sport_dict):
+    total_matches_dict = {}
+    matches = {}
+    index = 0
+    for sport in sport_dict:
+        total = 0
+        for group_id in sport_dict[sport]:
+            count = 0
+            previous_teams = []
+            for team in sport_dict[sport][group_id]:
+                count +=1
+                for team1 in sport_dict[sport][group_id]:
+                    if(team != team1 and team1 not in previous_teams):
+                        match = [team, team1, sport]
+                        matches[index] = match
+                        index+=1
+                previous_teams.append(team)
+            for i in range(count):
+                total += i
+
+
+        total_matches_dict[sport] = total
+
+    largest_sport = max(total_matches_dict, key=total_matches_dict.get)
+    time_slots = {}
+    for i in range(total_matches_dict[largest_sport]):
+        time_slots[i] = {}
+
+    return time_slots, matches
+
+def get_data(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    app = root.find('Sport')
+    sport_dict = gen_sport_dict(app)
+
+    time_slots, matches = gen_timeslot_dicts(sport_dict)
+
+    return sport_dict, time_slots, matches
+
+
+
+def run(pop_size, filename, generations = 10):
+    folder = "teams/"
+    sport_dict, time_slots, matches = get_data(folder+filename)
+    sport_list = list(sport_dict.keys())
+    population = ga.gen_pop(10,matches,time_slots,sport_list)
+    # counter = 0
+    # times = ["16:20", "17:00", "17:40", "18:20"]
+    # for time_slot in population[0]:
+    #     if time_slot % 4 == 0:
+    #         counter += 1
+    #     print('Day ', counter, ', ', times[time_slot % 4])
+    #     for sport in population[0][time_slot]:
+    #         if (population[0][time_slot][sport] == "Buffer"):
+    #             print("Buffer")
+    #         elif (population[0][time_slot][sport] != None):
+    #             i = population[0][time_slot][sport][0]
+    #             print(matches[i])
+
+    #least fit chromosome in initial_population and replace with fittest offspring
+    fitness_scores, overlaps = ga.fitness(population, matches)
+    fittest_idx, second_fittest_idx = ga.select(fitness_scores)
+
+    while(fitness_scores[fittest_idx] > 50):
+        fittest = population[fittest_idx]
+        second_fittest = population[second_fittest_idx]
+        print("fittest: ",fitness_scores[fittest_idx])
+
+        #Create 2 new children from the 2 fittest parents
+        child1, child2 = ga.crossover(fittest, second_fittest)
+        child1 = ga.mutation(child1, sport_list)
+        child2 = ga.mutation(child2, sport_list)
+    
+        #replace least fit with most fit's offspring
+        least_fit1, least_fit2 = ga.find_least_fit(fitness_scores)
+        population[least_fit1] = child1
+        population[least_fit2] = child2
+        for chromosome in range(len(population)):
+            population[chromosome] = ga.mutation(population[chromosome], sport_list)
+        fitness_scores, overlaps = ga.fitness(population, matches)
+        print("scores = ",fitness_scores)
+        fittest_idx, second_fittest_idx = ga.select(fitness_scores)
+
+    times = ["16:20", "17:00", "17:40", "18:20"]
+    counter = 0
+    for time_slot in population[fittest_idx]:
+        if time_slot % 4 == 0: counter+=1
+        print('Day ', counter, ', ', times[time_slot % 4])
+        for sport in population[fittest_idx][time_slot]:
+            if (population[fittest_idx][time_slot][sport] == "Buffer"):
+                print("Buffer")
+            elif (population[fittest_idx][time_slot][sport] != None):
+                i = population[fittest_idx][time_slot][sport][0]
+                print(matches[i])
+    for entry in overlaps[fittest_idx].keys():
+        print('Day ', int(entry / 4)+1, ', ', times[(entry-1) % 4], ' and ', times[entry % 4], ' contains conflict of team(s) ', overlaps[fittest_idx][entry])
+           
+
+print("Pop 10 Generations 100 on small")
+run(10, "teams.xml", 100)
+print("")
